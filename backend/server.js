e244fc72b0bb3c34c5dd3393343ec927b5ad27fe
Piyush55dude <piyushagarwalvo@gmail.com>
2021-06-01 23:49:17 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -52,10 +52,6 @@ const server = app.listen(
 
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
-  cors: {
-    origin: "http://localhost:3000",
-    // credentials: true,
-  },
 });
 
 io.on("connection", (socket) => {
