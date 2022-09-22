42253f09bf2f9bddb33537fdb5ff811b9d9b0b3a
kothawleprem <kothawleprem@gmail.com>
2022-09-22 18:24:44 +0530

    Resolved issue of incorrect users in dropdown while adding in chat group

--- frontend/src/components/miscellaneous/UpdateGroupChatModal.js
+++ frontend/src/components/miscellaneous/UpdateGroupChatModal.js
@@ -47,7 +47,6 @@ const UpdateGroupChatModal = ({ fetchMessages, fetchAgain, setFetchAgain }) => {
         },
       };
       const { data } = await axios.get(`/api/user?search=${search}`, config);
-      console.log(data);
       setLoading(false);
       setSearchResult(data);
     } catch (error) {
@@ -62,7 +61,7 @@ const UpdateGroupChatModal = ({ fetchMessages, fetchAgain, setFetchAgain }) => {
       setLoading(false);
     }
   };
-
+  
   const handleRename = async () => {
     if (!groupChatName) return;
 
