997e3f37bea1b233c888f02a348a31792ea4a2c1
HarshitSingh <geniousharshit@gmail.com>
2023-05-10 10:18:10 +0530

    Introduced Dark Mode

--- frontend/src/Pages/Chatpage.js
+++ frontend/src/Pages/Chatpage.js
@@ -15,7 +15,7 @@ const Chatpage = () => {
   
   return (
     <div style={{ width: "100%" }}>
-      {user && <SideDrawer />}
+      {user && <SideDrawer boxColor={boxColor}/>}
       <Box display="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px" >
         {user && <MyChats fetchAgain={fetchAgain} boxColor={boxColor} />}
         {user && (
