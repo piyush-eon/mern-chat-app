4968dae8129fddc3fb29489a9ea3c065af4f037a
Amit om kashyap <112419542+amitomksp@users.noreply.github.com>
2023-09-26 00:14:06 +0530

    Update db.js
      this code will add more connection options to the Mongoose connection, such as setting the useCreateIndex option to true to enable Mongoose to create indexes by default and this will add Error Handling for Invalid MONGO_URI: Before attempting to connect to the database, you can check if the MONGO_URI environment variable is defined and valid. If it's not, you can handle this case gracefully.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
-
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI not found in environment variables");
+    }
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false, 
     });
-
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.error(`Error: ${error.message}`.red.bold);
-    process.exit(1); // Exit with a non-zero status code to indicate an error
+    process.exit(1);
   }
 };
-
 module.exports = connectDB;
+
 
