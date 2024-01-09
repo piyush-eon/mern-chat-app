dbd544125980ce4683e4dd59b0a59b7c2589567e
Goru <abhiabhi9087@gmail.com>
2024-01-09 15:26:26 +0530

    fix-issue-44

--- frontend/src/components/userAvatar/UserListItem.js
+++ frontend/src/components/userAvatar/UserListItem.js
@@ -1,9 +1,7 @@
 import { Avatar } from "@chakra-ui/avatar";
 import { Box, Text } from "@chakra-ui/layout";
-import { ChatState } from "../../Context/ChatProvider";
 
-const UserListItem = ({ handleFunction }) => {
-  const { user } = ChatState();
+const UserListItem = ({ handleFunction, user }) => {
 
   return (
     <Box
