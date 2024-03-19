d8ee3e18d893c2126f1a72793997653129c1ab59
Shubhankar Srivastava <94240750+shubhankar132@users.noreply.github.com>
2024-03-20 01:14:36 +0530

    useHistory is not longer supported in react-router-dom v6
    Changes Made: 
    I have replaced 'useHistory' with 'useNavigate', 
    changed 'history = useHistory();' to 'navigate = useNavigate',
    replaced 'history.push()' with 'navigate()'|

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -5,7 +5,7 @@ import { VStack } from "@chakra-ui/layout";
 import { useState } from "react";
 import axios from "axios";
 import { useToast } from "@chakra-ui/react";
-import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom"; // useHistory is not supported in react-router-dom v6
 import { ChatState } from "../../Context/ChatProvider";
 
 const Login = () => {
@@ -16,7 +16,7 @@ const Login = () => {
   const [password, setPassword] = useState();
   const [loading, setLoading] = useState(false);
 
-  const history = useHistory();
+  const navigate = useNavigate();
   const { setUser } = ChatState();
 
   const submitHandler = async () => {
@@ -56,7 +56,7 @@ const Login = () => {
       setUser(data);
       localStorage.setItem("userInfo", JSON.stringify(data));
       setLoading(false);
-      history.push("/chats");
+      navigate("/chats");
     } catch (error) {
       toast({
         title: "Error Occured!",
